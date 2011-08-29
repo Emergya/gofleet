8c0f90d8b039aae4bc7f5c299d876342cce5f3b8
Jose M. Prieto <jmprieto@emergya.com>
2011-08-29 13:16:16 +0200

    sql: improvements for template_postgis creation
    Rename the file to create_template_postgis.sql
    Do not try to delete template_postgis: templates cannot be dropped.
    Get the Postgres share directory from the system to be version-independent.
    Grant permissions for geography_columns view.
    Add a VACUUM FULL.
    Do not allow connections to the template database.

---
+++ src/main/resources/create_template_postgis.sql
@@ -0,0 +1,28 @@
+-- Create the template database
+CREATE DATABASE template_postgis WITH TEMPLATE = template0 ENCODING = 'UTF8';
+\c template_postgis
+
+-- Add PLPGSQL language support
+CREATE OR REPLACE LANGUAGE plpgsql;
+
+---PostGIS
+\cd `pg_config --sharedir`
+\i ./contrib/postgis-1.5/postgis.sql
+\i ./contrib/postgis-1.5/spatial_ref_sys.sql
+
+---PgRoutingCore
+--\i /opt/pgrouting1.03/core/sql/routing_core.sql
+--\i /opt/pgrouting1.03/core/sql/routing_core_wrappers.sql
+
+-- Enabling users to alter spatial tables
+GRANT ALL ON geometry_columns TO PUBLIC;
+GRANT ALL ON geography_columns TO PUBLIC;
+GRANT ALL ON spatial_ref_sys TO PUBLIC;
+
+-- Garbage-collect and freeze
+VACUUM FULL;
+VACUUM FREEZE;
+
+-- Allows non-superusers the ability to create from this template
+UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template_postgis';
+UPDATE pg_database SET datallowconn = FALSE WHERE datname = 'template_postgis';
